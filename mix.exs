ef609d9b594ddd7b589cce94cf17aef6d86fb10f
Mike Bobadilla <mike.c.bobadilla@gmail.com>
2020-05-13 22:57:48 -0700

    install distillery

--- mix.exs
+++ mix.exs
@@ -45,7 +45,7 @@ defmodule ElixirChat.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:distillery, "~> 2.1", runtime: false}
+      {:distillery, "~> 2.1"}
     ]
   end
 
