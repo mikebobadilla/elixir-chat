95dad66f99ea1db010e14c78c395650da11ab00a
Mike Bobadilla <mike.c.bobadilla@gmail.com>
2020-05-13 22:45:30 -0700

    Added distillery

--- mix.exs
+++ mix.exs
@@ -44,7 +44,8 @@ defmodule ElixirChat.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:distillery, "~> 2.1", runtime: false}
     ]
   end
 
